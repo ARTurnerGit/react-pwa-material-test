b9f52cc197bb5bceeceb1fad90d9982831f3721d
Alun Turner <alun.turner@googlemail.com>
2020-05-17 17:01:32 +0100

    sticking on react googlemap interaction

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
 
-function MapView() {
-  return <div id="map"></div>;
+const mapStyles = {
+  width: "80%",
+  height: "70%",
+};
+
+function MapView(props) {
+  return (
+    <Map
+      google={props.google}
+      zoom={2}
+      style={mapStyles}
+      initialCenter={{ lat: 0, lng: 0 }}
+    />
+  );
 }
 
-export default MapView;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyBeyVA-bSzq5a-z2Cl_cM_35K068amyFtw",
+})(MapView);
